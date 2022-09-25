af8979af5a8d7a81edf44312ea4995d478744877
joakbjerk <joakbjerk@gmail.com>
2022-09-25 14:14:20 +0200

    Fixed capitalizeFirstLetter util function to acutally capitalize first letter

--- src/js/utils/text/text.utils.ts
+++ src/js/utils/text/text.utils.ts
@@ -1,6 +1,6 @@
 export function capitalizeFirstLetter(text: string): string {
-  const capitalizedFirstLetter = text.charAt(0);
-  const restOfText = text.slice(0);
+  const capitalizedFirstLetter = text.charAt(0).toLocaleUpperCase();
+  const restOfText = text.slice(1);
 
   return `${capitalizedFirstLetter}${restOfText}`;
 }
